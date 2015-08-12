80026ae6cb05eac35968aa49149dbffe6d27d705
C457 <android.c357@gmail.com>
2015-08-12 08:19:29 -0500

    lt02wifiue: update twrp flag and fix extract script

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,7 +112,7 @@ WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/mrvl/sd8787_uapsta.bin
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/mrvl/sd8787_uapsta.bin"
 
 # TWRP
-DEVICE_RESOLUTION := 1024x600
+TW_THEME := LANDSCAPE_MDPI
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_NO_USB_STORAGE := true
