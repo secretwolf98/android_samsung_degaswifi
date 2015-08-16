adeb1edf7ce0600f6d9f5726d141281c76d581f9
C457 <android.c357@gmail.com>
2015-08-16 06:11:56 -0500

    lt02wifiue: use opensource libxml2
    * Prebuilt version breaks audio due to old UCNV symbols

--- device.mk
+++ device.mk
@@ -87,6 +87,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    libxml2
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.pxa988 \
