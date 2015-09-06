fa7a42f43901632c4f694a61119cfa90e1329824
sub77 <sub77@ymail.com>
2015-09-06 12:59:28 +0000

    added consumerir.mrvl, libmarvellwireless, marvelwirelessdaemon, libhwcomposergc
    Change-Id: Ifb1b6c7e585a414f07abcf765deeaa1fb8630f6d

--- device.mk
+++ device.mk
@@ -54,6 +54,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnetcmdiface
 
+# IR
+PRODUCT_PACKAGES += \
+    consumerir.mrvl
+
+# Wireless
+PRODUCT_PACKAGES += \
+    libMarvellWireless \
+    MarvellWirelessDaemon
+
+# Graphics
+PRODUCT_PACKAGES += \
+    libHWComposerGC 
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     device/samsung/degaswifi/rootdir/fstab.pxa1088:root/fstab.pxa1088 \
